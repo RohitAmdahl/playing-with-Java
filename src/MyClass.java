77d8ecb24871da8b5601a51fb30fa8d1f282a8d3
rohitAmdahl <rohit_designer@outlook.com>
2024-02-28 00:23:10 +0100

    added new and resign the variable

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,11 +1,21 @@
 public class MyClass {
     public static void main(String[] args) {
-//        boolean gameOver = true;
-//        int score = 1200;
-//        int levelCompleted = 5;
-//        int bonus = 400;
-//        int finalScore = score;
-        calculateScore(true,500, 5, 100 );
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+        int finalScore = score;
+        calculateScore(gameOver,score,levelCompleted,bonus);
+
+         score = 10000;
+         levelCompleted = 8;
+         bonus = 200;
+        calculateScore(gameOver, score, levelCompleted,bonus );
+
+        score = 10;
+        levelCompleted = 2;
+        bonus = 2;
+        calculateScore(gameOver, score, levelCompleted,bonus );
     }
 
     public static void calculateScore(boolean gameOver, int score, int levelComplete, int bonus) {
